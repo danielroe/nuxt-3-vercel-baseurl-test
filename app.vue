aaced878e19bae1e06c0fd5f62023c30dce70023
Daniel Roe <daniel@roe.dev>
2024-05-23 17:32:43 +0100

    chore: init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const counter = ref(0)
+const isClient = ref(false)
+onMounted(() => {
+  isClient.value = true
+})
+</script>
+<template>
+  <div>
+    counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+  </div>
+  <div>
+    client script executed: {{ isClient }}
+  </div>
+</template>
